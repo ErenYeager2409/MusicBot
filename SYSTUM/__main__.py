a1d35e5d5c82d6eee7b8e1eda0c5e08f3e9dffaf
ErenYeager2409 <ps541268@gmail.com>
2025-01-06 13:52:47 +0530

    Update __main__.py

--- SYSTUM/__main__.py
+++ SYSTUM/__main__.py
@@ -53,17 +53,17 @@ if __name__ == "__main__":
 
 
 // server.mjs
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World!\n");
 });
 
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Listening on 127.0.0.1:3000");
 });
 
-// run with `node server.mjs`
+// run with "node server.mjs"
 
